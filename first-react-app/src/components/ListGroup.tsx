88794c0c894f2f181bd42040de211317fc441f3b
Akshat Vira <akshatvira812@gmail.com>
2023-07-11 17:34:53 +0530

    Add hook(s)

--- first-react-app/src/components/ListGroup.tsx
+++ first-react-app/src/components/ListGroup.tsx
@@ -1,7 +1,13 @@
-import { MouseEvent } from "react";
+// import { MouseEvent } from "react";
+import { useState } from "react";
 
 function ListGroup() {
   let items = ["Hello", "this is the 2nd line", "this is 3rd"];
+  //   let selectedIndex = -1;
+
+  //(State) Hook; tells that our vaiable would change with time
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
   //   items = [];
 
   //   const message = items.length == 0 ? <p>No item found</p> : null;
@@ -10,15 +16,25 @@ function ListGroup() {
   //     items.length == 0 ? <p>No item found</p> : null;
   //   };
   // Event handler
-  const handleClick = (event: MouseEvent) => console.log(event);
+  //   const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
       <h1>List</h1>
       {items.length == 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex == index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
