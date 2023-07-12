fa9cae715dbb30e9394496180706c0a5103e2b87
Akshat Vira <akshatvira812@gmail.com>
2023-07-12 11:49:29 +0530

    Add new components and complete the project

--- first-react-app/src/App.tsx
+++ first-react-app/src/App.tsx
@@ -1,18 +1,32 @@
-import ListGroup from "./components/ListGroup";
-function App() {
-  let items = ["Mumbai", "Delhi", "Bangalore"];
+// import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import { useState } from "react";
 
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
+function App() {
+  // let items = ["Mumbai", "Delhi", "Bangalore"];
+
+  // const handleSelectItem = (item: string) => {
+  //   console.log(item);
+  // };
+
+  // return (
+  //   <div>
+  //     <ListGroup
+  //       items={items}
+  //       heading="Cities"
+  //       onSelectItem={handleSelectItem}
+  //     />
+  //   </div>
+  // );
+
+  // Alerts
+  const [alertVisible, setAlertVisible] = useState(false);
 
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      <Button onClick={() => setAlertVisible(!alertVisible)}>My button</Button>
+      {alertVisible && <Alert>My alert</Alert>}
     </div>
   );
 }
